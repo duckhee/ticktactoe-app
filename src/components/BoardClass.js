31ebe871f311adbc04d3dc7f2e78f3727ec1c9df
doukheeWon <fain9301@candh.co.kr>
2023-04-28 17:49:29 +0900

    feat: props function send

--- src/components/BoardClass.js
+++ src/components/BoardClass.js
@@ -4,8 +4,27 @@ import "./Board.css";
 
 class BoardClass extends Component {
 
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            squares: Array(9).fill(null), // 배열의 시작부터 끝 인데스까지 해당 되는 값을 채워주는 함수가 fill 이다.
+        };
+    }
+
+    // 자식 Component에서 클릭 시 동작 시킬 function 정의
+    handleClick(i) {
+        const squares = this.state.squares.slice(); // 원본에는 영향을 주지 않는 상태에서 값을 가져오는 것 -> deep copy
+        squares[i] = 'X'; // 클리 시 값 넣어주기
+        // state 값을 변경
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <SquareClass value={i} aprops={i + 1}/>;
+        // return <SquareClass value={i} aprops={i + 1}/>;
+        // return <SquareClass value={i} />;
+        // 자식 Component에 값을 넣어준다.
+        return <SquareClass value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
     }
 
     render() {
--- src/components/SquareClass.js
+++ src/components/SquareClass.js
@@ -4,26 +4,30 @@ import "./square.css"
 class SquareClass extends React.Component {
 
 
-    constructor(props) {
-        super(props);
-        // useState와 동일한 기능을 하는 class 형 component의 기능
-        this.state = {
-            value: null,
-        };
+    /* constructor(props) {
+         super(props);
+         // useState와 동일한 기능을 하는 class 형 component의 기능
+         this.state = {
+             value: null,
+         };
 
-    }
+     }*/
 
     render() {
         return (
             <button className={"square"} onClick={() => {
-                console.log(`${this.props.value} - click`);
+                // console.log(`${this.props.value} - click`);
                 // useState에 값을 넣는 방법
-                this.setState({value: "X"});
+                // this.setState({value: "X"});
+                // 인자로 받은 props 함수를 호출하여 사용을 한다.
+                this.props.onClick()
             }}>
                 {/*props의 값을 가져오는 방법 -> 값의 이름이 value 이다.*/}
                 {/*{this.props.value}*/}
                 {/*useState의 값을 가져오는 방법*/}
-                {this.state.value}
+                {/*{this.state.value}*/}
+                {/*부모 Component에 있는 값을 가져오기 -> 상태 전파 */}
+                {this.props.value}
             </button>
         );
     }
