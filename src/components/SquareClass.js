98bcef79f7d0181e371d26e43497848832f3c841
doukheeWon <fain9301@candh.co.kr>
2023-04-28 13:14:11 +0900

    feat: click event add

--- src/components/SquareClass.js
+++ src/components/SquareClass.js
@@ -5,8 +5,8 @@ class SquareClass extends React.Component {
 
     render() {
         return (
-            <button className={"square"}>
-                {/*props의 값을 가져오는 방법*/}
+            <button className={"square"} onClick={() => console.log(`${this.props.value} - click`)}>
+                {/*props의 값을 가져오는 방법 -> 값의 이름이 value 이다.*/}
                 {this.props.value}
             </button>
         );
