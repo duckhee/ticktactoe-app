327a9b591a7a33990fb4080f5f65662671973836
doukheeWon <fain9301@candh.co.kr>
2023-04-28 13:20:59 +0900

    feat: class useState

--- src/components/SquareClass.js
+++ src/components/SquareClass.js
@@ -3,11 +3,27 @@ import "./square.css"
 
 class SquareClass extends React.Component {
 
+
+    constructor(props) {
+        super(props);
+        // useState와 동일한 기능을 하는 class 형 component의 기능
+        this.state = {
+            value: null,
+        };
+
+    }
+
     render() {
         return (
-            <button className={"square"} onClick={() => console.log(`${this.props.value} - click`)}>
+            <button className={"square"} onClick={() => {
+                console.log(`${this.props.value} - click`);
+                // useState에 값을 넣는 방법
+                this.setState({value: "X"});
+            }}>
                 {/*props의 값을 가져오는 방법 -> 값의 이름이 value 이다.*/}
-                {this.props.value}
+                {/*{this.props.value}*/}
+                {/*useState의 값을 가져오는 방법*/}
+                {this.state.value}
             </button>
         );
     }
