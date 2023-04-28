bde9a339140c1404cc94e58b7fc0c5381fd4c5bc
DoukheeWon <fain9301@yahoo.com>
2023-04-29 02:48:38 +0900

    feat: history back jump

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ import {useState} from "react";
 function App() {
     const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
     const [xIsNext, setXIsNext] = useState(true);
+    // 이전 값으로 넘어갔을 때, 스텝을 기억하기 위한 값을 useState에 저장
+    const [stepNumber, setStepNumber] = useState(0);
 
     const handleClick = (i) => {
-        const newSquares = currentState.squares.slice();
+        const newHistory = history.slice(0, stepNumber + 1);
+        const newCurrent = newHistory[newHistory.length - 1];
+        // const newSquares = currentState.squares.slice();
+        const newSquares = newCurrent.squares.slice();
         // 이미 클릭이 되어 있는 경우와 승자가 있을 경우 업데이트 하지 않음
         if (calculateWinner(newSquares) || newSquares[i]) {
             return;
         }
         newSquares[i] = xIsNext ? 'X' : 'O';
-        setHistory(prev => [...prev, {squares: newSquares}]); // 현재 저장된 히스토리 값을 가져와서 복사 한 후 추가를 해준다.
+        // setHistory(prev => [...prev, {squares: newSquares}]); // 현재 저장된 히스토리 값을 가져와서 복사 한 후 추가를 해준다.
+        setHistory([...newHistory, {squares: newSquares}]);
         setXIsNext(prevState => !prevState); // 이전 값을 읽어와서 값을 반전 시킨다.
+
+        setStepNumber(newHistory.length);
     };
 
 
@@ -44,17 +52,24 @@ function App() {
 
 
     // 현재 상태를 보여주기 위한 변수 선언
-    const currentState = history[history.length - 1];
+    // const currentState = history[history.length - 1];
+    const currentState = history[stepNumber];
 
     // 승자에 대한 계산 값
     const winner = calculateWinner(currentState.squares);
+
+    // 이전 값으로 돌아가는 함수
+    const jumpTo = (index) => {
+        setStepNumber(index);
+        setXIsNext((index % 2) == 0);
+    }
 
     // 이전 값으로 돌리기 위한 함수 선언
     const moves = history.map((step, index) => {
         const desc = index ? `Go to move #${index}` : `Go to Game Start`;
         return (
             <li key={index}>
-                <button>
+                <button onClick={() => jumpTo(index)}>
                     {desc}
                 </button>
             </li>
